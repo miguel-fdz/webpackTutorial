2d4b03981693d0fc6262a710cb56618cee74eb16
Miguel Fdz <miguelf.olalla@gmail.com>
2022-09-16 21:40:32 -0300

    Completed first three guides for webpack in their website guides

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import _ from 'lodash';
+import myName from './myName.js';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    const element2 = document.createElement('div');
+    element2.textContent = myName('Cody');
+
+    return [element, element2];
 }
 
-document.body.appendChild(component());
+let components = component();
+console.log(components);
+
+document.body.appendChild(components[0]);
+document.body.appendChild(components[1]);
